bbc6ef50fdc912d8735b59aeab126a40118c84b6
Patrick Snyder <2072916+pssnyder@users.noreply.github.com>
2025-06-15 22:14:09 -0400

    refactor: remove importmap script from index.html and update package.json to include @vitejs/plugin-react
    chore: update package-lock.json with new dependencies and versions
    
    fix: adjust vite.config.ts to ensure proper path resolution and include react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -10,8 +11,9 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve('.'),
         }
-      }
+      },
+      plugins: [react()]
     };
 });
